325f6e2e40cc3af2dc2f94b24704829ba6faad70
krinal <krinaljoshi8298@gmail.com>
2024-04-27 17:31:38 +0530

    added text cleaning module

--- data_cleaning/clean_data.py
+++ data_cleaning/clean_data.py
@@ -1,9 +1,16 @@
 import re
+import string
 
-def clean_text(text_str):
-  text_str = re.sub(r'^RT[\s]+', '', text_str) # Removes 'RT' tag from the tweet
-  text_str = re.sub(r'https?:\/\/.*[\r\n]*', '', text_str) # Removes hyperlinkls, starting with https
-  text_str = re.sub(r'#', '', text_str) # Removes the '#'
-  text_str = re.sub(r'[0-9]', '', text_str) # Removes all numerics
-  text_str = re.sub(r'[^A-Za-z0-9 ]+','',text_str) # Removes everything except alphabets and numerics
-  return text_str
+def clean(sample_text):
+    punctuations = string.punctuation # '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+    sample_text = re.sub(punctuations, " ") # removes punctuations
+    sample_text = re.sub(r'\w*\d\w*', '', sample_text) # returns alphanumerics
+    sample_text = re.sub(r'[#]+',' ', sample_text) # removes hash sign
+    sample_text = re.sub(r'[|]+',' ', sample_text) # removes pipe sign
+    sample_text = re.sub(r'\n+','', sample_text) # removes \n
+    sample_text = re.sub(r'[^\x00-\x7F]+',' ', sample_text) # removes non eng words
+    sample_text = re.sub(r"\bhttps\w+", "", sample_text) # removes word started with https
+    sample_text = re.sub(r"\bhttp\w+", "", sample_text) # removes word started with http
+    sample_text = re.sub(r'\s+', ' ', sample_text).strip() # removes spaces at beginning and ending
+    return sample_text
